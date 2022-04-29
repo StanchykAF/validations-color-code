4521964d10ebfa2a1bc25f82816e023d383fa1ca
Nastassia Stanchyk <stanchikaf@gmail.com>
2022-04-29 13:41:59 +0300

    Commit

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -5,9 +5,12 @@ import java.util.regex.Pattern;
 
 public class ColorCodeValidation {
     public static boolean validateColorCode(String color) {
-
-        // Put your code here
-
+        if (color == null) {
+            return false;
+        }
+        Pattern patternHexColor = Pattern.compile("(#)(([0-9A-F]{3})|([0-9A-F]{6}))", Pattern.CASE_INSENSITIVE);
+        Matcher matcherHexColor = patternHexColor.matcher(color);
+        return matcherHexColor.matches();
     }
 }
 
